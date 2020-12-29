cd0df32092898e6178660c005338a38d5d4db6ef
Park-Ji-Eun <penny0123@naver.com>
2020-12-30 04:00:39 +0900

    #4.1 createAction

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -3,12 +3,11 @@ import { connect } from "react-redux";
 import { actionCreators } from "../store";
 import { Link } from "react-router-dom";
 
-function ToDo({text, onBtnClick, id}) {
+function ToDo({ text, onBtnClick, id }) {
     return (
         <li>
-            <Link to={`/${id}`}>
-                {text} <button onClick={onBtnClick}>DEL</button>
-            </Link>
+            <Link to={`/${id}`}>{text}</Link>
+            <button onClick={onBtnClick}>DEL</button>
         </li>
     );
 }
